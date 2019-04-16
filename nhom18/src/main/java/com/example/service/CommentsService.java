f48c9ace72f24e1e17cd979c25b090a03d89a5fd
loser <racingboywin1998@gmail.com>
2019-04-16 19:37:25 +0700

    first commit

---
+++ nhom18/src/main/java/com/example/service/CommentsService.java
@@ -0,0 +1,9 @@
+package com.example.service;
+
+import java.util.List;
+
+import com.example.model.Comments;
+
+public interface CommentsService {
+	List<Comments> findAllCommentsByNewsDetails(int id);
+}
